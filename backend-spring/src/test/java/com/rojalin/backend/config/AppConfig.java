d77982fc038f76b13cc849a0864004601b68156e
Rojalin Shaw <rojalinshaw696@gmail.com>
2025-06-28 20:28:21 +0530

    Spring boot Backend Created and Spring Security add

---
+++ backend-spring/src/test/java/com/rojalin/backend/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.rojalin.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+
+public class AppConfig {
+	
+	private void SecurityFilterChain_SecurityFilterChain(HttpSecurity http)throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
