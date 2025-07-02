92fa6579a6b685f1c0becbda97c47de535a5ce53
Rojalin Shaw <rojalinshaw696@gmail.com>
2025-07-02 23:25:17 +0530

    Frontend Created

---
+++ frontend-react/tailwind.config.js
@@ -0,0 +1,11 @@
+// This is just temporary to make shadcn happy
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
