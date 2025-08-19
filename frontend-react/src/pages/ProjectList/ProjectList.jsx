20010329aeb8dc3fd769b16e25645a9e6460922e
Rojalin Shaw <rojalinshaw696@gmail.com>
2025-08-20 00:24:59 +0530

    project List Added

---
+++ frontend-react/src/pages/ProjectList/ProjectList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProjectList = () => {
+  return (
+  <>
+  <div className='relative px-5 lg:px-0 lg:flex gap-5 justify-center py-5'>
+    <section className='filterSection'>
+     <card className='p-5 sticky top-10'> 
+
+     </card>
+    </section>
+    <section className='projectListSection'>
+
+    </section>
+  </div>
+  </>
+  )
+}
+
+export default ProjectList
