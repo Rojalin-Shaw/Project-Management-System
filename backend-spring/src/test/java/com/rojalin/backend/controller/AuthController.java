955b4865af3f9f285038cf211910fb1f43975c7c
Rojalin Shaw <rojalinshaw696@gmail.com>
2025-10-07 23:00:54 +0530

    signin Request Created

--- backend-spring/src/test/java/com/rojalin/backend/controller/AuthController.java
+++ backend-spring/src/test/java/com/rojalin/backend/controller/AuthController.java
@@ -2,9 +2,14 @@ package com.rojalin.backend.controller;
 
 import java.security.PrivateKey;
 
+import javax.swing.Spring;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.webauthn.api.AuthenticatorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rojalin.backend.config.JwtProvider;
 import com.rojalin.backend.model.User;
 import com.rojalin.backend.repository.UserRepository;
 import com.rojalin.backend.service.CustomUserDetailsImpl;
+
+import response.AuthResponse;
 
 @RestController
 @RequestMapping("/auth")
@@ -39,6 +47,18 @@ public class AuthController {
 		createdUser.setEmail(user.getEmail());
 		createdUser.setFullname(user.getFullname());
 		User savedUser = userRepository.save(createdUser);
+		
+		UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = JwtProvider.generateToken(authentication);
+		
+		
+		AuthResponse res = new AuthResponse();
+		res.setMessage("signup success");
+		res.setJwt(jwt);
+		
 		return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
 	}
+	
+	public ResponseEntity<AuthResponse> signin(RequestBody)
 }
